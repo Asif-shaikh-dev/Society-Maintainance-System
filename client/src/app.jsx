8cbe1113df2700e4a579eeb93965715c8dd06281
Asif-shaikh-dev <asifsk.5070@gmail.com>
2025-06-13 17:48:06 +0530

    Rename RoomList component to RoomList2 and update import in App component

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import RoomList from './roomList.jsx';
+import RoomList2 from './RoomList.jsx';
 
 function App() {
   return (
     <div className="min-h-screen bg-gray-100 p-5">
       <h1 className="text-3xl font-bold mb-5">Society Maintenance System</h1>
-      <RoomList />
+      <RoomList2 />
     </div>
   );
 }
